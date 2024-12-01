8cd159f3c14676b8533b86a121962e21f5a6bcd2
jefersonprimer <jefersonpriimer@gmail.com>
2024-12-01 09:45:12 -0300

    refactor(Event class): refactored event class getters and setters

---
+++ src/main/java/com/eventostec/adapters/outbund/entities/JpaEventEntity.java
@@ -0,0 +1,33 @@
+package com.eventostec.api.adapters.outbound.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.uti
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class JpaEventEntity {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+    private String description;
+    private String imgUrl;
+    private String eventUrl;
+    private Boolean remote;
+    private Date date;
+}
