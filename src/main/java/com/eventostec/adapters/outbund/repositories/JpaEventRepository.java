d8ab7b1dd5b7ca5c018df5cf87603732897ece83
jefersonprimer <jefersonpriimer@gmail.com>
2024-12-01 09:54:15 -0300

    refactor(JpaEventRepository class): JpaRepository<Event, UUID> for JpaRepository<JpaEventEntity, UUID>

--- src/main/java/com/eventostec/adapters/outbund/repositories/JpaEventRepository.java
+++ src/main/java/com/eventostec/adapters/outbund/repositories/JpaEventRepository.java
@@ -12,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface EventRepository extends JpaRepository<JpaEventEntity, UUID> {
 
     @Query("SELECT e.id AS id, e.title AS title, e.description AS description, e.date AS date, e.imgUrl AS imgUrl, e.eventUrl AS eventUrl, e.remote AS remote, a.city AS city, a.uf AS uf " +
             "FROM Event e LEFT JOIN Address a ON e.id = a.event.id " +
